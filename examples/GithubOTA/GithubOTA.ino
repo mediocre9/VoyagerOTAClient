f7c7af2a8c6cd0f36085557a6b315ccd9e55cd4a
mediocre9 <mirzafahadzia9@gmail.com>
2026-03-01 02:47:58 +0500

    update examples according to new lib api changes

--- examples/GithubOTA/GithubOTA.ino
+++ examples/GithubOTA/GithubOTA.ino
@@ -1,8 +1,8 @@
 #define __ENABLE_ADVANCED_MODE__ true
 #define CURRENT_FIRMWARE_VERSION "1.0.0"
 
+#include <VoyagerOTAClient.h>
 #include <WiFi.h>
-#include <VoyagerOTA.hpp>
 
 using namespace Voyager;
 
@@ -15,12 +15,53 @@ void connectToWifi() {
     Serial.println("Connected to Internet");
 }
 
+struct GithubReleaseModel : public BaseModel {
+    String name;
+    String publishedAt;
+    int size;
+    int statusCode;
+
+    explicit GithubReleaseModel(String version, String downloadURL, String publishedAt, String name, int size, int statusCode)
+        : BaseModel(version, downloadURL) {
+        this->name = name;
+        this->publishedAt = publishedAt;
+        this->size = size;
+        this->statusCode = statusCode;
+    }
+};
+class GithubJSONParser : public Voyager::IParser<Voyager::HTTPResponseData, GithubReleaseModel> {
+public:
+    std::optional<GithubReleaseModel> parse(Voyager::HTTPResponseData responseData, int statusCode) override {
+        JsonDocument document;
+        DeserializationError error = deserializeJson(document, responseData);
+
+        if (error) {
+            Serial.println("JSON parsing failed");
+            return std::nullopt;
+        }
+
+        if (statusCode != HTTP_CODE_OK) {
+            return std::nullopt;
+        }
+
+        GithubReleaseModel payload(
+            document[0]["tag_name"],
+            document[0]["assets"][0]["url"],
+            document[0]["published_at"],
+            document[0]["name"],
+            document[0]["assets"][0]["size"].template as<int>(),
+            statusCode);
+
+        return payload;
+    }
+};
+
 void setup() {
     Serial.begin(9600);
     connectToWifi();
 
-    std::unique_ptr<GithubJSONParser> parser = std::make_unique<GithubJSONParser>();
-    OTA<HTTPResponseData, GithubReleaseModel> ota(std::move(parser), CURRENT_FIRMWARE_VERSION);
+    auto parser = std::make_unique<GithubJSONParser>();
+    OTA<HTTPResponseData, GithubReleaseModel> ota(CURRENT_FIRMWARE_VERSION, std::move(parser));
 
     // https://docs.github.com/en/rest/releases/releases?apiVersion=2022-11-28#:~:text=GET-,/repos/%7Bowner%7D/%7Brepo%7D/releases,-cURL
     std::vector<Header> releaseHeaders = {
@@ -29,8 +70,8 @@ void setup() {
         {"Accept", "application/vnd.github+json"},
     };
 
-    // replace {owner} and {repo} with your github username and repo.......
-    ota.setReleaseURL("https://api.github.com/repos/{owner}/{repo}/releases", releaseHeaders);
+    // replace with your github username and repo.......
+    ota.setReleaseURL("https://api.github.com/repos/{username}/{repo-name}/releases");
 
     Serial.println("OTA Started....");
 
@@ -38,7 +79,8 @@ void setup() {
 
     if (release && ota.isNewVersion(release->version)) {
         Serial.println("New version available: " + release->version);
-        Serial.println("Release name: " + release->name);
+        Serial.println("Release Name: " + release->name);
+        Serial.println("Release Date: " + release->publishedAt);
 
         std::vector<Header> downloadHeaders = {
             {"Authorization", "Bearer your-github-token..."},
@@ -46,7 +88,7 @@ void setup() {
             {"Accept", "application/octet-stream"},
         };
 
-        ota.setDownloadURL(release->browserDownloadUrl, downloadHeaders);
+        ota.setDownloadURL(release->downloadURL, downloadHeaders);
         ota.performUpdate();
     } else {
         Serial.println("No updates available yet!");
